ccd2aebacf0df4f6d7683b779a6c5910414af573
John S <johnc.sime@gmail.com>
2021-11-04 16:32:29 -0400

    Got task 3 & 4 fully working and passing

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const burger = {
 }
 
 // console.log(burger.discount("teacher"));
-console.log(burger.discount("student"));
+// console.log(burger.discount("student"));
 
 
 ///////////////Reviews (MVP)///////////////////
@@ -90,7 +90,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+// console.log(reviews[5].feedback);
 
 
 
@@ -99,7 +99,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-
+reviews[6].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
+console.log(reviews[6].feedback);
 
 
 
